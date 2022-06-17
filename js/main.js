33901f24b1bf54b73db15ffabe99f17a2d750aa6
unknown <josemachicote@hotmail.com>
2022-06-17 17:54:37 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*console.log("sacar un msg por la consola");
+ */
+
+console.log("se inicio mi js");
+
+//declaracion
+// var let const
+
+//const nombre = "Guille";
+
+
+/*let saludo = "hola";
+console.log(saludo);
+saludo = "hola mundo";
+console.log(saludo);*/
+
+let x = 10;
+let y = 15;
+let cartel1 = "hola";
+let cartel2 = "mundo";
+
+console.log(x + y);
+console.log(cartel1 + cartel2);
+
+let nombre = prompt("ingrese su nombre");
+let edad = prompt("ingrese su edad")
+
+console.log(nombre);
+console.log(edad);
+
+alertjose("Bienvenido " + nombre + "tu tienes" + edad + ".");
+alert("te felicito por inscribirte a este curso")
+
+
+/*let nombre = prompt("ingrese su nombre");
+let edad = prompt("ingrese su edad");
+
+alert("Bienvindo/a " + nombre + " tu tienes " + edad + ".");
+alert("Te felicito por inscribirte a este maravilloso curso");*/
