dbc99713207a8f1ac917be68721faca0a107cae3
unknown <josemachicote@hotmail.com>
2022-06-24 12:33:11 -0300

    agregando desafio

---
+++ js/desafio1.js
@@ -0,0 +1,3 @@
+let numero = prompt("ingrese un numero)")
+for (i = 1; i <= 10; i++)
+    console.log(numero + "x" + i + "=" + numero * i)
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*console.log("sacar un msg por la consola");
  */
 
-console.log("se inicio mi js");
+/*console.log("se inicio mi js");
 
 //declaracion
 // var let const
@@ -14,7 +14,7 @@ console.log(saludo);
 saludo = "hola mundo";
 console.log(saludo);*/
 
-let x = 10;
+/*let x = 10;
 let y = 15;
 let cartel1 = "hola";
 let cartel2 = "mundo";
@@ -29,7 +29,7 @@ console.log(nombre);
 console.log(edad);
 
 alertjose("Bienvenido " + nombre + "tu tienes" + edad + ".");
-alert("te felicito por inscribirte a este curso")
+alert("te felicito por inscribirte a este curso")*/
 
 
 /*let nombre = prompt("ingrese su nombre");
@@ -37,3 +37,97 @@ let edad = prompt("ingrese su edad");
 
 alert("Bienvindo/a " + nombre + " tu tienes " + edad + ".");
 alert("Te felicito por inscribirte a este maravilloso curso");*/
+
+/*if and else*/
+
+/*ejercicio1*/
+/*
+let nombre = prompt("Homero");
+let apellido = prompt("Simpson");
+let edad = prompt("39");
+console.log(nombre);
+console.log(apellido);
+console.log(edad);*/
+/*ejercicio2*/
+/*
+const ciudad1 = "montevideo";
+const ciudad2 = "tacuarembo";
+const ciudad3 = "minas";
+console.log(ciudad1);
+console.log(ciudad2);
+console.log(ciudad3);*/
+
+/*let entrada = prompt("Ingrese su nombre");
+let nombre = "Bart";
+if (entrada == nombre) { alert("Fui yo"); } else { alert("Yo no fui") };
+
+let tecla = prompt("Ingrese una tecla");
+if ((tecla == "Y") || (tecla == "y")) { alert("correcto"); } else { alert("incorrecto") };
+
+let numero = prompt("Ingrese un numero");
+if (numero == "1") { alert("elegiste a homero"); } else if (numero == "2") { alert("elegiste a bart"); } else if (numero == "3") {
+    alert(
+        "elegiste a marge"
+    );
+} else if (numero == "4") { alert("elegiste a raul"); } else { alert("gil"); }
+
+
+let numero3 = prompt("ingrese un numero");
+if ((numero3 >= 0) && (numero3 <= 1000)) { alert("presupuesto bajo"); } else if ((numero3 >= 1001) && (numero3 <= 3000)) { alert("presupuesto medio"); } else if (numero3 > 3000) { alert("presupuesto alto"); } else { alert("error"); }
+*/
+
+
+//es lo mismo
+//i = i + 1;
+// sintaxis sugar
+//i++;
+
+/*for (let i = 0; i <= 10; i++) {
+    console.log("i")
+}
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+//Ejericio de tablas//
+
+/*let tabla = 2;
+let resultado;
+
+for (let i = 1; i <= 10; i++) {
+    resultado = tabla * i;
+    console.log(tabla + " x " + i + " es igual a " + resultado);
+}
+
+/*for (let i = 3; i <= 7; i++) {
+
+    let ingresarNombre = prompt("Ingresar nombre");
+    alert(" Turno  N° " + i + " Nombre: " + ingresarNombre);
+}
+
+for (let i = 0; i < 10; i++) {
+    alert("indice: " + i);
+}*/
+
+/*let myAge = 32
+let myRatio = 7
+
+let myDogAge = myAge * myRatio
+console.log(myDogAge)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)*/
+
+function increment() {
+    console.log("hiciste click en la consola")
+}
