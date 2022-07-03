d8ef6016a546e809dcfe6462e0832ffd9a271cbb
unknown <josemachicote@hotmail.com>
2022-07-03 11:13:03 -0300

    agregando ciclos

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ for (let i = 1; i <= 10; i++) {
 let total = 0
 
 function agregarGuitarra() {
-    const numero = prompt("Ingrese el numero de la guitarra a agregar")
+    const numero = prompt("Ingrese el numero del instrumento a agregar")
     switch (numero) {
         case "1":
             total = total + 500;
@@ -25,6 +25,12 @@ function agregarGuitarra() {
 
         case "4":
             total = total + 650;
+            break;
+        case "5":
+            total = total + 700;
+            break;
+        case "6":
+            total = total + 750;
             break;
 
         default:
@@ -41,7 +47,7 @@ function comprar() {
 
     if (total == 0) {
         alert
-            ("todavia no has seleccionado prodcuto")
+            ("todavia no has seleccionado producto")
     } else if (total >= 700) {
         alert("Gracias por tu compra.\n El total es $" + total + "\n Te regalamos el envio")
     } else { alert("Gracias por tu compra.\n El total es $" + (total + 50) + " envio incluido") }
